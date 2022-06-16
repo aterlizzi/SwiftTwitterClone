66c534de2ce45a8e951d7d1c3bfdebc5eb2472a7
Aidan Terlizzi <act5@rice.edu>
2022-06-16 10:39:41 -0400

    Initial Commit

---
+++ SwiftTwitterClone/SwiftTwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTwitterCloneApp.swift
+//  SwiftTwitterClone
+//
+//  Created by Aidan Terlizzi on 6/16/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
